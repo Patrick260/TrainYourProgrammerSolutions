31aabbddbc17c11ca7a4f73633ed401663f2fdce
Patrick260 <76070679+Patrick260@users.noreply.github.com>
2021-04-09 20:50:00 +0200

    Create FunktionWelcheMinimumUndMaximumAusEinemArrayZurückgibt.java

---
+++ Java/Exercise_15/FunktionWelcheMinimumUndMaximumAusEinemArrayZurückgibt.java
@@ -0,0 +1,28 @@
+package de.patrick260.trainYourProgrammer.exercise_15;
+
+public class FunktionWelcheMinimumUndMaximumAusEinemArrayZurückgibt {
+
+    public static void main(String[] args) {
+        int[] array = {1,4,8,2,0,3,6};
+        System.out.println("Min: " + getMinMax(array)[0] + " Max: " + getMinMax(array)[1]);
+    }
+
+    private static int[] getMinMax(int[] array) {
+
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < array.length; i++) {
+
+            if (array[i] < min) {
+                min = array[i];
+            } else if (array[i] > max) {
+                max = array[i];
+            }
+
+        }
+
+        int[] minMax = {min, max};
+        return minMax;
+    }
+}
