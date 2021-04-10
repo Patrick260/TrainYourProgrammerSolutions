51bd204356e20d6df6bda32345bc6f40d6da6feb
Patrick260 <76070679+Patrick260@users.noreply.github.com>
2021-04-10 21:25:47 +0200

    Create DurchschnittlicheNiederschlagsmenge.java

---
+++ java/Exercise_19/DurchschnittlicheNiederschlagsmenge.java
@@ -0,0 +1,16 @@
+package de.patrick260.trainYourProgrammer.exercise_19;
+
+public class DurchschnittlicheNiederschlagsmenge {
+
+    public static void main(String[] args) {
+        int april = 12;
+        int mai = 14;
+        int juni = 8;
+
+        System.out.println("Niederschlag im April: " +  april);
+        System.out.println("Niederschlag im Mai: " +  mai);
+        System.out.println("Niederschlag im Juni: " +  juni);
+        System.out.println("Durchschnitt: " + (double)(april+mai+juni)/3);
+    }
+
+}
